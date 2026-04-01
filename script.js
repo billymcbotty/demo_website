3357f183ff02efbf33809583e220d44433d80846
billymcbotty <billymcbotty@gmail.com>
2026-03-31 21:51:15 -0700

    Add OpenClaw explainer demo site

---
+++ script.js
@@ -0,0 +1,12 @@
+const navToggle = document.querySelector('.nav-toggle');
+const nav = document.querySelector('.nav');
+
+if (navToggle && nav) {
+  navToggle.addEventListener('click', () => {
+    nav.classList.toggle('open');
+  });
+
+  nav.querySelectorAll('a').forEach((link) => {
+    link.addEventListener('click', () => nav.classList.remove('open'));
+  });
+}
